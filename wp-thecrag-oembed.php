c05ff84b2fcad5f49e584bef7aebf9a0155e30a4
Simon Dale <scd@thecrag.com>
2014-09-26 08:30:38 +1000

    getting started

---
+++ wp-thecrag-oembed.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WP theCrag oEmbed
+ * Description: converts theCrag links to embedded content in your WordPress site
+ * Version: 1.0
+ * Author: Simon Dale
+ * Author URI: http://www.thecrag.com/climber/scd
+ * License: GPL2
+ *
+ */
+
+
+
+?>
