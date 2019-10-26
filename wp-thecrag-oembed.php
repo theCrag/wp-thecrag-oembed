f41ed474f10d8f5be55fb9480c3d8dbf7b25f42c
Brendan Heywood <brendan@catalyst-au.net>
2019-10-26 22:59:42 +1100

    Simplified oembed to essentially try any url at all

--- wp-thecrag-oembed.php
+++ wp-thecrag-oembed.php
@@ -11,17 +11,8 @@
  */
 
 function add_oembed_thecrag(){
-/* oembeds to production server */
-  wp_oembed_add_provider( '#https?://(www\.)?thecrag\.com/photo/.*#i','https://www.thecrag.com/oembed',true);
-  wp_oembed_add_provider( '#https?://(www\.)?thecrag\.com/competition/.*#i','https://www.thecrag.com/oembed',true);
-  wp_oembed_add_provider( '#https?://(www\.)?thecrag\.com/climber/(.+)/stream/mine#i','https://www.thecrag.com/oembed',true);
-  wp_oembed_add_provider( '#https?://(www\.)?thecrag\.com/(.+/)?(ascents|routes|areas|nodes|photos|topos|circuits|activity|climbers|favorites|stream)(/.*)?#i','https://www.thecrag.com/oembed',true);
-
-/* oembeds to development server for testing purposes */
-  wp_oembed_add_provider( '#https?://brendan\.thecrag\.com/photo/.*#i','https://dev.thecrag.com/oembed',true);
-  wp_oembed_add_provider( '#https?://brendan\.thecrag\.com/competition/.*#i','https://dev.thecrag.com/oembed',true);
-  wp_oembed_add_provider( '#https?://brendan\.thecrag\.com/climber/(.+)/stream/mine#i','https://dev.thecrag.com/oembed',true);
-  wp_oembed_add_provider( '#https?://brendan\.thecrag\.com/(.+/)?(ascents|routes|areas|nodes|photos|topos|circuits|activity|climbers|favorites|stream)(/.*)?#i','https://dev.thecrag.com/oembed',true);
+  wp_oembed_add_provider( '#https?://(www\.)?thecrag\.com/.*#i','https://www.thecrag.com/oembed',true);
+  wp_oembed_add_provider( '#https?://brendan\.thecrag\.com/.*#i','https://dev.thecrag.com/oembed',true);
 }
 
 
